725b99e3f7b871bf07f364e4092c16abee4ac7d5
j-parrott <jonathan.parrott@infinityworks.com>
2021-07-14 14:55:20 +0100

    add access control to cloudfront url

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -28,6 +28,7 @@ export class CdkStack extends cdk.Stack {
       "jfp-static-react-app-distribution",
       {
         defaultBehavior: { origin: new origins.S3Origin(bucket) },
+        webAclId: `arn:aws:wafv2:us-east-1:${process.env.AWS_ACCOUNT}:global/webacl/${process.env.WEB_ACL_ID}`,
       }
     );
 
